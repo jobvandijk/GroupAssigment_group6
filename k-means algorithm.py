41d4a52e3b97b22fd3c76717e302b47be35a2737
WoutRutten <w.t.rutten@student.tue.nl>
2024-06-15 13:54:14 +0200

    reworking and naming the code's I have written

--- k-means algorithm.py
+++ k-means algorithm.py
@@ -3,14 +3,18 @@ from sklearn.datasets    import make_blobs
 from sklearn.cluster import KMeans
 import numpy as np
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 
 # Load the data
-file_path = r"C:\Users\20223095\OneDrive - TU Eindhoven\Documents\GitHub\GroupAssigment_group6\tested_molecular_desc.csv"
+file_path = r"sorted_tested_molecules.csv"
 df = pd.read_csv(file_path)
-print (df.isna().any(axis=1))
+df_smiles = pd.read_csv(r"tested_molecules_original.csv")
+df["SMILES"] = df_smiles["SMILES"]
+#print(df)
+#print (df.isna().any(axis=1))
 df1 = df[df.isna().any(axis=1)]
-print (df1)
+#print (df1)
 
 
 # Define the features and target variables
@@ -22,35 +26,29 @@ X = X.astype(np.float32)
 y = y.astype(np.float32)
 
 # Split dataset into training set and test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
 kmeans = KMeans(n_clusters=20,random_state=0)
-y_pred = kmeans.fit_predict(X_train)
+y_pred = kmeans.fit_predict(X)
+
+df['Cluster'] = y_pred
+
 
 results_df = pd.DataFrame({
-    'Molecule': df.loc[y_pred.index, 'SMILES'],
+    'Molecule': df['SMILES'],
     'PKM2_actual': df['PKM2_inhibition'].values,
     'ERK2_actual': df['ERK2_inhibition'].values,
     'Cluster': y_pred
 })
-df['Cluster'] = y_pred
-print(results_df.head())
+print(results_df)
+print(df)
+
+plt.scatter(results_df['PKM2_actual'], results_df['ERK2_actual'], c=results_df['Cluster'], cmap='viridis')
+plt.xlabel('PKM2 Inhibition')
+plt.ylabel('ERK2 Inhibition')
+plt.title('K-means Clustering of Molecules based on Kinase Inhibition')
+plt.show()
 
 
-#voorbeeld dataframe
-X, _ = make_blobs(n_samples=10, centers=3, n_features=4)
-df = pd.DataFrame(X, columns=['Feat_1', 'Feat_2', 'Feat_3', 'Feat_4'])
 
-kmeans = KMeans(n_clusters=3,random_state=0)
-
-y = kmeans.fit_predict(df[['Feat_1', 'Feat_2', 'Feat_3', 'Feat_4']])
-df['Cluster'] = y
-
-
-y_other = kmeans.fit_predict(df.iloc[:,:4])
-
-
-df['Other'] = y_other
-
-print(df.head())
 
