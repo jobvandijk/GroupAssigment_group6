92217672ecc293e3d5e80d475f4bcf4b78bad50e
jobvandijk <j.a.v.dijk@student.tue.nl>
2024-06-18 12:53:24 +0200

    descriptor calculation clear

--- functions.py
+++ functions.py
@@ -1,12 +1,10 @@
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Descriptors
+from rdkit.Chem import MACCSkeys
 
 df = pd.read_csv('untested_molecules.csv')
-def compute_2Ddescriptors(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    descriptors = [desc[1](mol) for desc in Descriptors.descList if not desc[0].startswith('fr_')]
-    return descriptors
+#---Dataframe for MACCSKeys---
 """
 def get_maccs_keys(smiles):
     # Convert SMILES to RDKit molecule
@@ -21,9 +19,17 @@ def get_maccs_keys(smiles):
 # Apply the function to the 'SMILES' column and create a new DataFrame for MACCS keys
 maccs_keys_df = df['SMILES'].apply(get_maccs_keys)
 maccs_keys_df = pd.DataFrame(maccs_keys_df.tolist(), columns=[f'MACCS_{i}' for i in range(1, 167)])"""
+#-----------------------------------------------------------------------------------------------
+
+#---Dataframe for 2D descriptors----------------
+def compute_2Ddescriptors(smiles):
+    mol = Chem.MolFromSmiles(smiles)
+    descriptors = [desc[1](mol) for desc in Descriptors.descList if not desc[0].startswith('fr_')]
+    return descriptors
 # Compute descriptor values for each SMILES string and add as new columns
 descriptor_columns = [desc[0] for desc in Descriptors.descList if not desc[0].startswith('fr_')]
 df[descriptor_columns] = df['SMILES'].apply(lambda x: pd.Series(compute_2Ddescriptors(x)))
 
-print(df.  head())
-df.to_csv('untested_2D_descriptors.csv', index=False)
+#print(df.head())
+#df.to_csv('untested_2D_descriptors.csv', index=False)
+#------------------------------------------------------------------------
