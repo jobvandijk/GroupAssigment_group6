33bbc17d2682210ee2179f2f26c03d07a735ca34
Stefan_vd_Vuurst <151642392+Stefan-vd-vuurst@users.noreply.github.com>
2024-06-18 15:48:11 +0200

    I think finalised

--- functions_neural_network.py
+++ functions_neural_network.py
@@ -121,7 +121,7 @@ def plot_confusion_matrix(predictions, outp_val, mol_name='', single=False):
         print("Balanced Accuracy:", balanced_accuracy_score(y_true, y_pred))
         print("Confusion Matrix:", cm_plot(y_true, y_pred, class_name=mol_name))
 
-    else:        
+    elif single==False:        
         y_pred_PKM2 = predictions[:, 0]
         y_pred_ERK2 = predictions[:, 1]
         
@@ -178,11 +178,13 @@ def train_model_individual(descriptor_array, outputs_array, untested_data, mol_n
     predictions = model.predict(descr_val) 
     predictions = np.round(predictions).astype(int)  
 
-    print("descriptor array:",descriptor_array.shape)
-    print("untested array:", untested_data.shape)
-
     untested_predictions = model.predict(untested_data)
     untested_predictions = np.round(predictions).astype(int)
+
+    if early_stopping.stopped_epoch == None or early_stopping.stopped_epoch == 0:
+        best_epoch = 200
+    else:
+        best_epoch = early_stopping.stopped_epoch - early_stopping.patience
 
     if plot_conf_mat == True:
         plot_confusion_matrix(predictions, outp_val, mol_name, single=True)   
@@ -203,7 +205,7 @@ def train_model_individual(descriptor_array, outputs_array, untested_data, mol_n
     print("Number of 1 in", mol_name, ": ", np.count_nonzero(outputs_array[:] == 1), " so percentage of 1 in", mol_name, ": ", np.count_nonzero(outputs_array[:] == 1)/outputs_array.shape[0])
     print("Number of predicted 1s for ", mol_name, ": ", np.count_nonzero(predictions == 1), "\n") 
 
-    print("Number of epochs before early stopping: ", early_stopping.stopped_epoch - early_stopping.patience)
+    print("Number of epochs before early stopping: ", best_epoch, "\n")
 
     if plot_loss == True:
         plot_network_loss_progression(training_history)
@@ -211,8 +213,11 @@ def train_model_individual(descriptor_array, outputs_array, untested_data, mol_n
     return predictions, untested_predictions
 
 
-def main_struct_individual(input_train_file, input_test_file, output_file, balance_weights=[], plot_loss=False, MACCS=False, write_output=False, plot_conf_mat=False):
+def main_struct_individual(input_train_file, input_test_file, output_file, balance_weights=[], plot_loss=False, MACCS=False, write_output=False, plot_conf_mat=False, seed_val=10):
     # Main structure for the method of individual networks.
+
+    np.random.seed(seed_val)
+    tf.random.set_seed(seed_val)
 
     df = read_file(input_train_file)
     untested_df = read_file(input_test_file)
@@ -242,11 +247,17 @@ def main_struct_individual(input_train_file, input_test_file, output_file, balan
     pkm2_output = outp_arr[:,0]
     erk2_output = outp_arr[:,1]
 
-    predictions_PKM2, untested_predictions_PKM2 = train_model_individual(descr_arr, pkm2_output, untested_descr_arr, mol_name='PKM2', balance_weights=balance_weights[0], plot_loss=True, plot_conf_mat=True)
-    predictions_ERK2, untested_predictions_ERK2 = train_model_individual(descr_arr, erk2_output, untested_descr_arr, mol_name='ERK2', balance_weights=balance_weights[0], plot_loss=True, plot_conf_mat=True)
+    if descr_arr.shape[1] < untested_descr_arr.shape[1]:
+        untested_descr_arr = untested_descr_arr[:, :descr_arr.shape[1]]
+
+    predictions_PKM2, untested_predictions_PKM2 = train_model_individual(descr_arr, pkm2_output, untested_descr_arr, mol_name='PKM2', balance_weights=balance_weights[0], plot_loss=plot_loss, plot_conf_mat=plot_conf_mat)
+    predictions_ERK2, untested_predictions_ERK2 = train_model_individual(descr_arr, erk2_output, untested_descr_arr, mol_name='ERK2', balance_weights=balance_weights[0], plot_loss=plot_loss, plot_conf_mat=plot_conf_mat)
 
     predictions = np.column_stack((predictions_PKM2, predictions_ERK2))  
     untested_predictions = np.column_stack((untested_predictions_PKM2, untested_predictions_ERK2)) 
+
+    print("\nNumber of untested PKM2 1 predictions: ", np.count_nonzero(untested_predictions[:,0] == 1))
+    print("Number of untested ERK2 1 predictions: ", np.count_nonzero(untested_predictions[:,1] == 1)) 
 
     output_data = format_output(untested_predictions, untested_df, output_cols)
 
@@ -324,9 +335,6 @@ def train_model_four_classes(descriptor_array, outputs_array, untested_descr, ba
     early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
 
     balancing_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(outputs_array), y=outputs_array)
-    balancing_weights[1] = balancing_weights[1]/2
-    balancing_weights[2] = balancing_weights[2]/2
-    balancing_weights[3] = balancing_weights[3]/2
     balancing_weights_dict = dict(enumerate(balancing_weights))
 
     if balance_weights == True:
@@ -346,8 +354,11 @@ def train_model_four_classes(descriptor_array, outputs_array, untested_descr, ba
     untested_predictions = np.argmax(untested_predictions, axis=1)
 
     if plot_conf_mat == True:
-        two_class_outp_val = convert_four_to_two_classes(outp_val)
+        outp_val_four_class = np.argmax(outp_val, axis=1)
+
+        two_class_outp_val = convert_four_to_two_classes(outp_val_four_class)
         two_class_pred = convert_four_to_two_classes(class_predictions)
+
         plot_confusion_matrix(two_class_pred, two_class_outp_val)
 
     if early_stopping.stopped_epoch == None or early_stopping.stopped_epoch == 0:
@@ -377,7 +388,7 @@ def train_model_four_classes(descriptor_array, outputs_array, untested_descr, ba
     print("Number of predicted 2s for: ", np.count_nonzero(class_predictions == 2)) 
     print("Number of predicted 3s for: ", np.count_nonzero(class_predictions == 3), "\n")             
 
-    print("Number of epochs before early stopping: ", best_epoch)
+    print("Number of epochs before early stopping: ", best_epoch, "\n")
 
     if plot_loss == True:
         plot_network_loss_progression(training_history)
@@ -385,15 +396,16 @@ def train_model_four_classes(descriptor_array, outputs_array, untested_descr, ba
     return class_predictions, untested_predictions
 
 
-def main_struct_four_class(input_file, input_test_file, output_file,  balance_weights=[], plot_loss=False, MACCS=False, write_output=False, plot_conf_mat=False):
+def main_struct_four_class(input_file, input_test_file, output_file,  balance_weights=[], plot_loss=False, MACCS=False, write_output=False, plot_conf_mat=False, seed_val=10):
+
+    np.random.seed(seed_val)
+    tf.random.set_seed(seed_val)
 
     df = read_file(input_file)
     untested_df = read_file(input_test_file)
 
     output_cols = ['PKM2_inhibition', 'ERK2_inhibition']
     mol_col = ['SMILES']
-
-    untested_descr = split_data(untested_df, output_cols, mol_col)
 
     if balance_weights[0]==False:
         dupe_amount = balance_weights[1]
@@ -406,15 +418,25 @@ def main_struct_four_class(input_file, input_test_file, output_file,  balance_we
         descr_arr = np.array(maccs_keys_string_to_list)
         outp_arr = np.array(df[["PKM2_inhibition", "ERK2_inhibition"]].values.tolist())
 
+        maccs_keys_untested = [ast.literal_eval(string_list) for string_list in untested_df["maccs_keys_bitstring"]]
+        untested_descr_arr = np.array(maccs_keys_untested)        
     elif MACCS==False:
         descr_arr, outp_arr = split_data(balanced_df, output_cols, mol_col)
 
+        untested_descr_arr, untested_outp = split_data(untested_df, output_cols, mol_col)
+
     four_class_data = convert_two_to_four_classes(outp_arr)
 
-    four_class_predictions, four_class_untested_predictions = train_model_four_classes(descr_arr, four_class_data, untested_descr, balance_weights=balance_weights[0], plot_loss=plot_loss, plot_conf_mat=plot_conf_mat)
+    if descr_arr.shape[1] < untested_descr_arr.shape[1]:
+        untested_descr_arr = untested_descr_arr[:, :descr_arr.shape[1]]
+
+    four_class_predictions, four_class_untested_predictions = train_model_four_classes(descr_arr, four_class_data, untested_descr_arr, balance_weights=balance_weights[0], plot_loss=plot_loss, plot_conf_mat=plot_conf_mat)
 
     two_class_predictions = convert_four_to_two_classes(four_class_predictions) 
     two_class_untested_pred = convert_four_to_two_classes(four_class_untested_predictions)
+
+    print("\nNumber of untested PKM2 1 predictions: ", np.count_nonzero(two_class_untested_pred[:,0] == 1))
+    print("Number of untested ERK2 1 predictions: ", np.count_nonzero(two_class_untested_pred[:,1] == 1))    
 
     output_data = format_output(two_class_untested_pred, untested_df, output_cols, mol_col)
 
